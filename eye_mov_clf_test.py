4a1642789b1ce8e2f3051e5b4b7abe20448c4cda
Junsang <junsangjasonpark@gmail.com>
2022-04-06 11:07:26 +0900

    noise_reducing

--- eye_mov_clf_test.py
+++ eye_mov_clf_test.py
@@ -20,14 +20,30 @@ def px2deg(screen_size, distance, screen_resolution):
     """
     return degrees(atan2(0.5 * screen_size, distance)) / (0.5 * screen_resolution)
 
-def gap_fill_in(data):
-    data = pd.read_csv(data)
+def load_data(file_name):
+    data = pd.read_csv(file_name)
     ts, x, y = np.array(data['ts']), np.array(data['x']), np.array(data['y'])
+
+    return ts, x, y
+
+
+def gap_fill_in(ts, x, y):
+    """
+    Until now, this function is loading the data. However, soon it will be seperated from this function.
+
+    ts(ms) = timestamp
+    x(px) = x coordinate
+    y(px) = y coordinate
+    max_gap_length(ms) = the duration between a last valid point before the gap and a last point in the gap
+
+    """
+    ts, x, y = ts, x, y
+    
     max_gap_length = 75
     
     nan_index = np.argwhere(np.isnan(x))
-    gaps = []
     gap = False
+    gaps = []
     temp_gap = []
     
     for i in nan_index:
@@ -56,10 +72,13 @@ def gap_fill_in(data):
                 x[index] = linear_interpolation(cor_data_x, ts[index])
                 y[index] = linear_interpolation(cor_data_y, ts[index])
                 
-    return x, y, ts
+    return ts, x, y
 
 def linear_interpolation(data, target_x):
     """
+    The gap is filled by utilizing linear_interpolation function.
+    This is the basic way to interpolate missing data.
+
     data = list([data_x_1, data_y_1], [data_x_2, data_y_2])
     target_x = x from missing value
     """
@@ -68,3 +87,30 @@ def linear_interpolation(data, target_x):
                 * (target_x - data[1][0]) + data[1][1]
     
     return target_y
+
+def noise_reducing(nums, window, method = 'med'):
+    
+    """
+    This function reduces noises of data using median or average method.
+    Default is median for following a guideline of Tobii.
+    """
+    reduced_nums = np.array([])
+    if method == 'avg':
+        for i in range(len(nums)):
+            if np.isnan(nums[i]):
+                reduced_nums = np.append(reduced_nums, nums[i])
+            elif i < window or i >= (len(nums) - window):
+                reduced_nums = np.append(reduced_nums, nums[i])
+            else:
+                reduced_nums = np.append(reduced_nums,
+                                            np.nanmean(nums[i-window:i+window]))
+    elif method == 'med':
+        for i in range(len(nums)):
+            if np.isnan(nums[i]):
+                reduced_nums = np.append(reduced_nums, nums[i])
+            elif i < window or i >= (len(nums) - window):
+                reduced_nums = np.append(reduced_nums, nums[i])
+            else:
+                reduced_nums = np.append(reduced_nums,
+                                            np.nanmedian(nums[i-window:i+window]))
+    return reduced_nums
