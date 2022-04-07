a8c72b01899159b922cb3e2d557fc51bfbd2df9c
Junsang <junsangjasonpark@gmail.com>
2022-04-07 16:34:21 +0900

    'add_function'

--- eye_mov_clf_test.py
+++ eye_mov_clf_test.py
@@ -1,9 +1,9 @@
-from math import atan2, degrees
 import pandas as pd
 import numpy as np
+import math
 from matplotlib import pyplot as plt
 
-def px2deg(screen_size, distance, screen_resolution):
+def pixel_to_degree(screen_size, distance, screen_resolution):
     """
     px2deg converts pixels of coordinates to degrees.
     It helps eye movement classifiers work based on degrees.
@@ -19,16 +19,21 @@ def px2deg(screen_size, distance, screen_resolution):
     
     ex) px2deg(60, 70, 1920)
     """
-    return degrees(atan2(0.5 * screen_size, distance)) / (0.5 * screen_resolution)
+    px2deg = math.degrees(math.atan2(0.5 * screen_size, distance)) / (0.5 * screen_resolution)
+    return px2deg
 
 def load_data(file_name):
     data = pd.read_csv(file_name)
     ts, x, y = np.array(data['ts']), np.array(data['x']), np.array(data['y'])
+    coordinate_data = {
+        'Timestamp': ts,
+        'X':x,
+        'Y': y
+    }
+    
+    return coordinate_data
 
-    return ts, x, y
-
-
-def gap_fill_in(ts, x, y):
+def gap_fill_in(coordinate_data):
     """
     ts(ms) = timestamp
     x(px) = x coordinate
@@ -36,7 +41,7 @@ def gap_fill_in(ts, x, y):
     max_gap_length(ms) = the duration between a last valid point before the gap and a last point in the gap
 
     """
-    ts, x, y = ts, x, y
+    ts, x, y = coordinate_data['Timestamp'], coordinate_data['X'], coordinate_data['Y']
     
     max_gap_length = 75
     
@@ -70,8 +75,14 @@ def gap_fill_in(ts, x, y):
             for index in gap:
                 x[index] = linear_interpolation(cor_data_x, ts[index])
                 y[index] = linear_interpolation(cor_data_y, ts[index])
-                
-    return ts, x, y
+    
+    coordinate_data = {
+        'Timestamp': ts,
+        'X':x,
+        'Y': y
+    }
+    
+    return coordinate_data
 
 def linear_interpolation(data, target_x):
     """
@@ -81,68 +92,73 @@ def linear_interpolation(data, target_x):
     data = list([data_x_1, data_y_1], [data_x_2, data_y_2])
     target_x = x from missing value
     """
-    target_y = ((data[1][0] - data[0][0]) \
-                / (data[1][1] - data[0][1])) \
+    target_y = ((data[0][1] - data[1][1]) \
+                / (data[0][0] - data[1][0])) \
                 * (target_x - data[1][0]) + data[1][1]
     
     return target_y
 
-def noise_reducing(nums, window, method = 'med'):
+def noise_reducing(coordinate_data, window, method = 'med'):
     
     """
     This function reduces noises of data using median or average method.
     Default is median for following a guideline of Tobii.
     """
-    reduced_nums = np.array([])
-    if method == 'avg':
-        for i in range(len(nums)):
-            if np.isnan(nums[i]):
-                reduced_nums = np.append(reduced_nums, nums[i])
-            elif i < window:
-                for j in reversed(range(i+1)):
-                    if j != 0:
-                        reduced_nums = np.append(reduced_nums, np.nanmean(nums[i-j:i+j]))
-                        break
-                    else:
-                        reduced_nums = np.append(reduced_nums, nums[i])
-            elif i >= (len(nums) - window):
-                for j in reversed(range(len(nums) - i)):
-                    if j != 0:
-                        reduced_nums = np.append(reduced_nums, np.nanmean(nums[i-j:i+j]))
-                        break
-                    else:
-                        reduced_nums = np.append(reduced_nums, nums[i])
-            else:
-                reduced_nums = np.append(reduced_nums, np.nanmean(nums[i-window:i+window]))
-    elif method == 'med':
-        for i in range(len(nums)):
-            if np.isnan(nums[i]):
-                reduced_nums = np.append(reduced_nums, nums[i])
-            elif i < window:
-                for j in reversed(range(i+1)):
-                    if j != 0:
-                        reduced_nums = np.append(reduced_nums, np.nanmedian(nums[i-j:i+j]))
-                        break
-                    else:
-                        reduced_nums = np.append(reduced_nums, nums[i])
-            elif i >= (len(nums) - window):
-                for j in reversed(range(len(nums) - i)):
-                    if j != 0:
-                        reduced_nums = np.append(reduced_nums, np.nanmedian(nums[i-j:i+j]))
-                        break
-                    else:
-                        reduced_nums = np.append(reduced_nums, nums[i])
-            else:
-                reduced_nums = np.append(reduced_nums, np.nanmedian(nums[i-window:i+window]))
-    return reduced_nums
+    for key, coordinate in coordinate_data.items():
+        if key == 'Timestamp':
+            pass
+        reduced_coordinate = np.array([])
+        if method == 'avg':
+            for i in range(len(coordinate)):
+                if np.isnan(coordinate[i]):
+                    reduced_coordinate = np.append(reduced_coordinate, coordinate[i])
+                elif i < window:
+                    for j in reversed(range(i+1)):
+                        if j != 0:
+                            reduced_coordinate = np.append(reduced_coordinate, np.nanmean(coordinate[i-j:i+j]))
+                            break
+                        else:
+                            reduced_coordinate = np.append(reduced_coordinate, coordinate[i])
+                elif i >= (len(coordinate) - window):
+                    for j in reversed(range(len(coordinate) - i)):
+                        if j != 0:
+                            reduced_coordinate = np.append(reduced_coordinate, np.nanmean(coordinate[i-j:i+j]))
+                            break
+                        else:
+                            reduced_coordinate = np.append(reduced_coordinate, coordinate[i])
+                else:
+                    reduced_coordinate = np.append(reduced_coordinate, np.nanmean(coordinate[i-window:i+window]))
+        elif method == 'med':
+            for i in range(len(coordinate)):
+                if np.isnan(coordinate[i]):
+                    reduced_coordinate = np.append(reduced_coordinate, coordinate[i])
+                elif i < window:
+                    for j in reversed(range(i+1)):
+                        if j != 0:
+                            reduced_coordinate = np.append(reduced_coordinate, np.nanmedian(coordinate[i-j:i+j]))
+                            break
+                        else:
+                            reduced_coordinate = np.append(reduced_coordinate, coordinate[i])
+                elif i >= (len(coordinate) - window):
+                    for j in reversed(range(len(coordinate) - i)):
+                        if j != 0:
+                            reduced_coordinate = np.append(reduced_coordinate, np.nanmedian(coordinate[i-j:i+j]))
+                            break
+                        else:
+                            reduced_coordinate = np.append(reduced_coordinate, coordinate[i])
+                else:
+                    reduced_coordinate = np.append(reduced_coordinate, np.nanmedian(coordinate[i-window:i+window]))
+        coordinate_data[key] = reduced_coordinate
+        
+    return coordinate_data
 
 def drawing_seperate_scatter(coordinate_data):
     """
-    coordinate_data = dict('X coordinate': x, 'Y coordinate': y, 'Timestamp': ts)
+    coordinate_data = dict('Timestamp': ts, 'X': x, 'Y': y)
     """
     legend = ['X coordinate', 'Y coordinate']
-    plt.scatter(coordinate_data['Timestamp'], coordinate_data['X coordinate'])
-    plt.scatter(coordinate_data['Timestamp'], coordinate_data['Y coordinate'])
+    plt.scatter(coordinate_data['Timestamp'], coordinate_data['X'])
+    plt.scatter(coordinate_data['Timestamp'], coordinate_data['Y'])
     plt.xlabel('Timestamp')
     plt.ylabel('Coordinate')
     plt.legend(legend)
@@ -150,9 +166,25 @@ def drawing_seperate_scatter(coordinate_data):
 
 def drawing_xy_scatter(coordinate_data):
     """
-    coordinate_data
+    data = dict('X coordinate': x, 'Y coordinate': y, 'Timestamp': ts)
     """
-    plt.scatter(coordinate_data['X coordinate'], coordinate_data['Y coordinate'], color = 'skyblue')
-    plt.xlabel('X coordinate')
-    plt.ylabel('Y coordinate')
-    return plt.show() 
+    plt.scatter(coordinate_data['X'], coordinate_data['Y'], color = 'skyblue')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    return plt.show()
+
+def get_angular_velocity(coordinate_data, px2deg):
+    x = coordinate_data['X']
+    y = coordinate_data['Y']
+    ts = coordinate_data['Timestamp']
+    distance = np.array([np.NaN])
+    
+    for i in range(len(x)-1):
+        degree = px2deg *  math.sqrt(((x[i+1] - x[i]) ** 2) + ((y[i+1] - y[i]) ** 2))
+
+        distance = np.append(
+            distance,
+            (degree / (ts[i+1] - ts[i]) * 1000)
+        )
+        
+    return distance
